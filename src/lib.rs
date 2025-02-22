fd3b6f772617313e61dd2dc259af37a4e99ad999
hitohata <hirohatatro@gmail.com>
2025-02-22 10:32:46 -0800

    update insert

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 pub mod models;
 pub mod schema;
 
-use diesel::prelude::*;
+use diesel::{prelude::*, sqlite::Sqlite};
 use dotenvy::dotenv;
+use models::{NewPost, Post};
 use std::env;
 
 pub fn establish_connection() -> SqliteConnection {
@@ -11,4 +12,17 @@ pub fn establish_connection() -> SqliteConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &mut SqliteConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .returning(Post::as_returning())
+        .get_result(conn)
+        .expect("Error saving new post")
+
 }
